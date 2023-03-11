9a9e7f6b545dd2207be1cf730e92a802457f3449
Darren LaCour <lacourd@icloud.com>
2023-03-10 23:42:18 -0600

    finished chapter reading

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,16 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      let errorMsg = '';
+      if (newTitle === "") {
+         errorMsg = "Please enter a title";
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your list`;
+      } else if(!this.movies.includes(newTitle)){
+         this.movies.push(newTitle);
+      }
+      return errorMsg 
+   }
 }
