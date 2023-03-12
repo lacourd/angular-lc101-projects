f3204cc10f181dee9df2489054009f8717b62db0
Darren LaCour <lacourd@icloud.com>
2023-03-12 15:15:26 -0500

    chapter exercises completed

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,16 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(candidate : object) {
+    if (!this.crew.includes(candidate)){
+      this.crew.push(candidate);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
+  changeMissionName(userInput: string) {
+    this.missionName = userInput;
+    return false;
+  }
 
 }
